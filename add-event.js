751163e3a02e1906f3f27936f7d98c2515faf68e
Kyle Combes <kylecombes@gmail.com>
2017-06-14 14:07:23 -0400

    Implementing adding of event using AJAX. Working except for calling AJAX error function instead of success.

--- add-event.js
+++ add-event.js
@@ -4,12 +4,36 @@ $(document).ready(function () {
         var title = $('#event-title').val();
         var startDate = $('#start-date').val();
         var startTime = $('#start-time').val();
+        var start = new Date(startDate + " " + startTime);
         var endDate = $('#end-date').val();
         var endTime = $('#end-time').val();
+        var end = new Date(endDate + " " + endTime);
         var location = $('#location').val();
         var description = $('#description').val();
         var visibility = $('input[name=privacy]:checked').val();
-        alert("You pressed my button :)");
+        var data = {
+            title: title,
+            start: start,
+            end: end,
+            location: location,
+            description: description,
+            visibility: visibility
+        };
+        $.ajax({
+            url: 'http://localhost:4000/calendarUpdate',
+            type: 'POST',
+            contentType: 'text/plain', //'application/json;charset=utf-8',
+            data: JSON.stringify(data),
+            success: function( data ){
+                alert("Event saved")
+            },
+            error: function( jqXHR, textStatus, errorThrown ){
+                alert(jqXHR.responseText)
+                // alert("Error: Status: "+textStatus+" Message: "+errorThrown);
+            }
+        }).catch(function (data, status, headers, config) {
+            alert(data)
+        });
 
     });
 
