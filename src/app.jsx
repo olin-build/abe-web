ac52c8fb12bdb81b9c9dce6803844ca5e39fbdfb
Kyle Combes <kylecombes@gmail.com>
2017-06-19 12:18:49 -0400

    Initial setup of React with Wepack server-side compiler.

---
+++ src/app.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import {render} from 'react-dom';
+
+class PageHeaderTitle extends React.Component {
+    render() {
+        return <h1>Welcome to ABE!</h1>
+
+    }
+}
+
+class PageHeaderNav extends React.Component {
+    render() {
+        return <p>A nav menu should go here...</p>
+    }
+}
+
+class PageHeader extends React.Component {
+    render() {
+        return (
+            <header>
+                <PageHeaderTitle/>
+                <PageHeaderNav/>
+            </header>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return <PageHeader/>
+    }
+}
+
+render(<App/>, document.getElementById('app'));
