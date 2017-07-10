f7c201d8401347a4bf8ac4ef0eaf3cf7dbb4cb3a
audreywl <audrey.lewis@students.olin.edu>
2017-07-10 15:23:57 -0400

    all the necessary pieces on the import scene, nothing tested yet

--- src/scenes/Import/import.jsx
+++ src/scenes/Import/import.jsx
@@ -6,9 +6,12 @@ import TagEntry from '../../components/tag-entry.jsx';
 export default class ImportScene extends React.Component {
   constructor(props) {
       super(props);
-      this.labelsChanged = this.labelsChanged.bind(this)
+      this.labelsChanged = this.labelsChanged.bind(this);
+      this.urlChanged = this.urlChanged.bind(this);
+      this.submitICS = this.submitICS.bind(this);
       this.state = {
         labels : [],
+        url : '',
       }
       this.possibleLabels = ['Library', 'OFAC', 'FWOP', 'OARS', 'Robolab', 'StAR', 'PGP', 'Admission', "Candidates' Weekend"]
     }
@@ -19,12 +22,32 @@ export default class ImportScene extends React.Component {
           this.setState(state);
       }
   }
+  urlChanged(e) {
+      let state = this.state;
+      state = Object.assign(state, {url: e.currentTarget.value});
+      this.setState(state);
+  }
+  submitICS(){
+    $.ajax({
+        url: window.abe_url + '/ics',
+        method: POST,
+        contentType: 'application/json',
+        data: JSON.stringify(this.state),
+        success: response => alert('ICS imported'),
+        error: function( jqXHR, textStatus, errorThrown ){
+            alert("Error: " + errorThrown);
+        },
+      })
+}
   render(){
     return(
       <div className="row expanded page-container">
           <div className="row content-container">
               <h1 className="page-title">Import</h1>
+              <input required="required" type="url" placeholder="...example.com/example_calendar.ics" className="wide-text-box single-line-text-box medium-text-box" onChange={this.urlChanged}/>
               <TagEntry tags={this.state.labels} onChange={this.labelsChanged} possibleLabels={this.possibleLabels}/>
+              <br/>
+              <input type="submit" className="button submit" value="Submit" onClick={this.submitICS}/>
           </div>
       </div>
     )
