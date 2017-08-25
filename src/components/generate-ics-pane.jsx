3212d1773bd6abf9e8b6bd901045317f9605aeb1
Kyle Combes <kylecombes@gmail.com>
2017-08-25 17:22:55 -0400

    Added link to new GitHub Wiki guides to Subscribe (ICS feed) pane.

--- src/components/generate-ics-pane.jsx
+++ src/components/generate-ics-pane.jsx
@@ -21,15 +21,15 @@ export default class GenerateICSPane extends React.Component {
     render() {
         return (
             <div>
-                <span className="sidebar-item-info">Use an ICS feed to add events with the selected tags to your own calendar.</span>
+                <span className="sidebar-item-info">Use an ICS feed to add events with the selected tags to your own calendar (
+                    <a href="https://github.com/olinlibrary/abe-web/wiki/Integrate-with-Your-Calendar"
+                       alt="Instructions for adding an ICS feed"
+                       title="Instructions for adding an ICS feed"
+                       target="_blank">
+                        instructions
+                    </a>).</span>
                 <a className="ics-copy-to-clipboard" title="Copy feed URL" alt="Copy feed URL" onClick={this.copyToClipboard}>Copy link to clipboard</a>
-                <span className="ics-instructions">Instructions for adding to:&nbsp;
-                    <a href="https://support.google.com/calendar/answer/37100" alt="Add Olin event calendar to Google Calendar"
-                       title="Add Olin event calendar to Google Calendar" target="_blank">Google Calendar</a> ("Add using a link"),&nbsp;
-                    <a href="https://support.office.com/en-us/article/Import-or-subscribe-to-a-calendar-in-Outlook-com-or-Outlook-on-the-web-CFF1429C-5AF6-41EC-A5B4-74F2C278E98C"
-                       title="Add Olin event calendar to Outlook desktop or Outlook.com" alt="Add Olin event calendar to Outlook desktop or Outlook.com" target="_blank">
-                        Outlook</a> (follow second half on subscribing).
-                </span>
+
             </div>
         )
     }
