5b55039d0df9424ac0cb83e74f522275eb86dace
Eric Miller <eric@legoaces.org>
2018-04-19 01:09:24 -0400

    Added MVP functionality to subscription page

--- src/pages/subscription/subscription.jsx
+++ src/pages/subscription/subscription.jsx
@@ -10,50 +10,62 @@ export default class SubscriptionEditorPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            importData: {
+            data: {
                 labels : [],
-                url : '',
+                id : '',
             },
         };
+
+        Object.assign(this.state.data, this.getIdFromURL(props));
+
         this.props.setSidebarMode(SidebarModes.IMPORT);
         props.setPageTitlePrefix('Edit Subscription');
     }
 
+    getIdFromURL = (props) => {
+        if ('match' in props && 'id' in props.match.params) {
+            return {id: props.match.params.id};
+        }
+        return null;
+    };
+
     labelToggled = (labelName) => {
-        let labels = this.state.importData.labels.slice();
+        let labels = this.state.data.labels.slice();
         if (labels.includes(labelName)) {
             labels.splice(labels.indexOf(labelName),1);
         } else {
             labels.push(labelName);
         }
-        this.setState({importData: Object.assign({}, this.state.importData, {labels})});
+        this.setState({data: Object.assign({}, this.state.data, {labels})});
       };
 
-      urlChanged = (e) => {
-          let importData = this.state.importData;
-          importData = Object.assign(importData, {url: e.currentTarget.value});
-          this.setState({importData : importData});
-      };
+      // urlChanged = (e) => {
+      //     let importData = this.state.importData;
+      //     importData = Object.assign(importData, {url: e.currentTarget.value});
+      //     this.setState({importData : importData});
+      // };
 
-    submitICS = () => {
-        axios.post(window.abe_url + '/ics/', this.state.importData)
+    submitSubscription = () => {
+      console.log(this.state);
+      axios.put(window.abe_url + '/subscriptions/'+this.state.data.id, this.state.data)
           .then(
             (response) => this.props.importSuccess(response, this.state.importData),
             (jqXHR, textStatus, errorThrown) => this.props.importFailed(errorThrown, jqXHR.message)
           );
     };
 
-      render(){
-        return(
-          <div className="row expanded page-container">
-              <div className="row content-container">
-                  <h1 className="page-title"><MenuIconButton onClick={this.props.toggleSidebarCollapsed} tooltip="Show/Hide Sidebar"/>Import</h1>
-                  <input required="required" type="url" placeholder=".../example_calendar.ics" className="wide-text-box single-line-text-box medium-text-box" onChange={this.urlChanged}/>
-                  <TagPane contentClass='import-filters' {...this.props} possibleLabels={this.props.labels} selectedLabels={this.state.importData.labels} labelToggled={this.labelToggled}/>
-                  <br/>
-                  <input type="submit" className="button submit" value="Submit" onClick={this.submitICS}/>
-              </div>
-          </div>
-        )
-      }
+    render(){
+      return(
+        <div className="row expanded page-container">
+            <div className="row content-container">
+                <h1 className="page-title"><MenuIconButton onClick={this.props.toggleSidebarCollapsed} tooltip="Show/Hide Sidebar"/>Edit Subscription: {this.state.data.id}</h1>
+                {/*<input required="required" type="url" placeholder=".../example_calendar.ics" className="wide-text-box single-line-text-box medium-text-box" onChange={this.urlChanged}/>*/}
+                <h2>Tags: </h2>
+                <TagPane contentClass='import-filters' {...this.props} possibleLabels={this.props.labels} selectedLabels={this.state.data.labels} labelToggled={this.labelToggled}/>
+                <br/>
+                <input type="submit" className="button submit" value="Submit" onClick={this.submitSubscription}/>
+            </div>
+        </div>
+      )
+    }
 }
