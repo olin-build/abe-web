0b8292c7e7559acf33170d37e4253e3bd62a6f55
Oliver Steele <steele@osteele.com>
2018-05-03 12:49:50 -0400

    Fix remaining lint warnings in touched files

--- src/components/menu-icon-button.jsx
+++ src/components/menu-icon-button.jsx
@@ -4,17 +4,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MenuIconSVG from '../../assets/menu-icon.svg';
 
-export default class MenuIconButton extends React.Component {
-  render() {
-    return (
-      <button className="menu-icon-button" onClick={this.props.onClick} title={this.props.tooltip}>
+const MenuIconButton = props => (
+      <button className="menu-icon-button" onClick={props.onClick} title={props.tooltip}>
         <MenuIconSVG />
       </button>
     );
-  }
-}
 
 // Define React prop types for type checking during development
 MenuIconButton.propTypes = {
   onClick: PropTypes.func.isRequired,
 };
+
+export default MenuIconButton;
