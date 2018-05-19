c500a3091ccce692d230e1d70ae238ec3fca38ad
Oliver Steele <steele@osteele.com>
2018-05-19 14:06:47 -0400

    Mock moment.now()

--- src/__test__/setup.js
+++ src/__test__/setup.js
@@ -6,6 +6,7 @@ configure({ adapter: new Adapter() });
 jest.mock('moment', () => {
   const moment = require.requireActual('moment-timezone');
   moment.tz.setDefault('America/New_York');
+  moment.now = () => 1526697000000;
   return moment;
 });
 
