95f02fbbfcd6b02c87be3bd9d108e64704a9ef62
Oliver Steele <steele@osteele.com>
2018-06-04 16:16:26 -0400

    SSLify didn't work on Heroku because router -> server is always HTTP

--- server/sslify.js
+++ server/sslify.js
@@ -2,12 +2,19 @@
 const ACME_CHALLENGE_PATH = '/.well-known/acme-challenge/';
 
 // skip rewriting this request?
-const skip = req => req.secure || req.path.startsWith(ACME_CHALLENGE_PATH);
+const skipRedirect = (req) => {
+  const remoteProto = req.headers['x-forwarded-proto'];
+  // Prefer the header over req.secure. The client->lb connection differ in
+  // either direction from the lb -> server connection.
+  const proto = remoteProto || (req.secure ? 'https' : 'https');
+  // Avoid recursive redirects, and forwarding ACME challenges.
+  return proto === 'https' || req.path.startsWith(ACME_CHALLENGE_PATH);
+};
 
 // rewrite this request's URL as HTTPS
 const rewrite = req => `https://${req.get('Host')}${req.url}`;
 
 // ExpressJS middleware
-const sslify = (req, res, next) => (skip(req) ? next() : res.redirect(rewrite(req)));
+const sslify = (req, res, next) => (skipRedirect(req) ? next() : res.redirect(rewrite(req)));
 
 module.exports = sslify;
