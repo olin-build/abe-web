bad4b9374eb05d6aa9548802884d757c9c82cbff
Kyle Combes <kcombes@indigoag.com>
2020-09-29 22:13:18 -0400

    Log env

--- src/data/client.js
+++ src/data/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { API_SERVER_URL } from './settings';
 
+console.log(`API URL from env: ${API_SERVER_URL}`);
+
 const apiClient = axios.create({
   baseURL: API_SERVER_URL,
 });
